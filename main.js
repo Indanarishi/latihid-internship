8f68f0d720175d210c6edb5040c65cc75999f48e
Jundi <60133872+Indanarishi@users.noreply.github.com>
2020-09-18 23:25:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,3 @@ window.addEventListener("scroll", function(){
     var nav = document.querySelector(".nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
-
-function myFunction() {
-    var x = document.getElementById("links");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
